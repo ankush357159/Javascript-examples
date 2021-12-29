728764bb1af5e3f78a7fc4391405e1537bd7cc5f
Ankush <ankush357159@gmail.com>
2021-12-29 22:49:56 +0530

    map function

---
+++ map.js
@@ -0,0 +1,11 @@
+const myUsers = [
+    { name: 'shark', likes: 'ocean' },
+    { name: 'turtle', likes: 'pond' },
+    { name: 'otter', likes: 'fish biscuits' }
+]
+
+const items = myUsers.map(item=> {
+    console.log(item.name) 
+})
+
+// console.log(myUsers);
